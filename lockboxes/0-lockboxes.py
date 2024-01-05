e2bcd815ade08b47f3df3c083729fb7db244759a
Romain BAYLE <5014@holbertonstudents.com>
2024-01-05 16:22:36 +0000

    fixed pep8

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -10,23 +10,25 @@ There can be keys that do not have boxes.
 The first box boxes[0] is unlocked.
 
 Args:
-	boxes (list): list of lists
+    boxes (list): list of lists
 
 Return: True if all boxes can be opened, else return False
 """
-def canUnlockAll(boxes):
-	# Try to validate each key except 0
-	for key in range(1, len(boxes)):
-		flag = False
-		# Iterate through all boxes
-		for box in range(len(boxes)):
-			# If box contain the current key and isn't the current box
-			if key in boxes[box] and box != key:
-				flag = True
-				break
-		# Key wasn't found in another box
-		if not flag:
-			return False
 
-	# Each keys have been found in another box, so all boxes could be read
-	return True
+
+def canUnlockAll(boxes):
+    # Try to validate each key except 0
+    for key in range(1, len(boxes)):
+        flag = False
+        # Iterate through all boxes
+        for box in range(len(boxes)):
+            # If box contain the current key and isn't the current box
+            if key in boxes[box] and box != key:
+                flag = True
+                break
+        # Key wasn't found in another box
+        if not flag:
+            return False
+
+    # Each keys have been found in another box, so all boxes could be read
+    return True
