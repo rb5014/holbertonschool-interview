53b1dcc2de4803f273c568d2c7e15b962ae72894
Romain BAYLE <5014@holbertonstudents.com>
2024-01-05 16:13:50 +0000

    fixed doc for module

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
+
+"""You have n number of locked boxes in front of you.
+Each box is numbered sequentially from 0 to n - 1 and
+each box may contain keys to the other boxes.
+
+All keys will be positive integers.
+There can be keys that do not have boxes.
+
+The first box boxes[0] is unlocked.
+
+Args:
+	boxes (list): list of lists
+
+Return: True if all boxes can be opened, else return False
+"""
 def canUnlockAll(boxes):
-	"""You have n number of locked boxes in front of you.
-	Each box is numbered sequentially from 0 to n - 1 and
- 	each box may contain keys to the other boxes.
-  
-	All keys will be positive integers.
-	There can be keys that do not have boxes.
- 
-	The first box boxes[0] is unlocked>
- 
-	Args:
-		boxes (list): list of lists
-  
-	Return: True if all boxes can be opened, else return False
-	"""
 	# Try to validate each key except 0
 	for key in range(1, len(boxes)):
 		flag = False
