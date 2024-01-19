dbd0b90e84a38e6c13b3a38e7782aec6f34e9d22
Romain BAYLE <5014@holbertonstudents.com>
2024-01-19 10:26:01 +0000

    0. New node

---
+++ heap_insert/1-heap_insert.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * heap_insert - inserts a value into a Max Binary Heap
+ * If the address stored in root is NULL,
+ * the created node must become the root node.
+ * Have to respect a Max Heap ordering.
+ * Allowed to have up to 6 functions in file.
+ * @root: double pointer to the root node of the Heap
+ * @value: value to store in the node to be inserted
+ * Return: return a pointer to the inserted node, or NULL on failure
+*/
+/*heap_t *heap_insert(heap_t **root, int value)
+{
+
+}*/
