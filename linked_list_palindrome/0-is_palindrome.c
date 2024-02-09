f1f734846f7130f4db30fda3b8601f3003bccd1d
BAYLE Romain <5014@holbertonstudents.com>
2024-02-09 10:05:03 +0000

    Fixed muitiple edge cases for palindrome, like empty list, 1 node, long list, odd numper of nodes, etc

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -9,20 +9,33 @@
 
 int is_palindrome(listint_t **head)
 {
-	int values[100], nb_values = 0;
+	int values[10000], nb_values = 0;
 	listint_t *current = *head;
 
 
-	if (*head == NULL)
-		return (0);
+	if ((*head == NULL) || ((*head)->next == NULL))
+		return (1);
 
 	while (current != NULL)
 	{
 		nb_values++;
 		values[nb_values - 1] = current->n;
 		current = current->next;
-		if (current->n == values[nb_values - 1])
+		if (current && (current->n == values[nb_values - 1]))
 		{
+			while ((current != NULL) && (nb_values > 0))
+			{
+				if (current->n != values[nb_values - 1])
+					return (0);
+				nb_values--;
+				current = current->next;
+			}
+			return (1);
+		}
+		else if (current && (nb_values - 2 >= 0) &&
+				 (current->n == values[nb_values - 2]))
+		{
+			nb_values--;
 			while ((current != NULL) && (nb_values > 0))
 			{
 				if (current->n != values[nb_values - 1])
