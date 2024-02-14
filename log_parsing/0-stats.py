919b7a901a5a6c9a9969e2b029a4ffed214d70f5
Bayle Romain <5014@holbertonstudents.com>
2024-02-14 16:57:29 +0000

    added try except to 0-stats.py

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -4,11 +4,16 @@
 Module 0-stats
 Exercice Log parsing
 """
+import traceback
+import sys
+
+
 
 def main():
     """
     main
     """
+
     tot_file_size = 0
 
     # Dictionary status codes
@@ -20,25 +25,35 @@ def main():
                 '404': 0,
                 '405': 0,
                 '500': 0}
-    count = 0
-    while 1:
-        count += 1
-        input_string = input()
-        sep_string = input_string.split(" ")
-        # Add filesize to total size
-        tot_file_size += int(sep_string[-1])
-        if sep_string[-2] in status_dict.keys():
-            # Increment the count for the respective status code
-            status_dict[sep_string[-2]] += 1
+    try:
+        count = 0
+        while 1:
+            count += 1
+            input_string = input()
+            sep_string = input_string.split(" ")
+            # Add filesize to total size
+            tot_file_size += int(sep_string[-1])
+            if sep_string[-2] in status_dict.keys():
+                # Increment the count for the respective status code
+                status_dict[sep_string[-2]] += 1
 
-        # Print each ten lines
-        if count == 10:
-            count = 0
-            print("File size: " + str(tot_file_size))
-            for status in sorted(status_dict.keys()):
-                if status_dict[status] > 0:
-                    print("{}: {}".format(status, status_dict[status]))
-                    status_dict[status] = 0
+            # Print each ten lines
+            if count == 10:
+                count = 0
+                print_info(tot_file_size, status_dict)
+    except:
+        """ Keyboard interruption """
+        print_info(tot_file_size, status_dict)
+
+
+def print_info(tot_file_size, status_dict):
+    """ print_info """
+    print("File size: " + str(tot_file_size))
+    for status in sorted(status_dict.keys()):
+        if status_dict[status] > 0:
+            print("{}: {}".format(status, status_dict[status]))
+            status_dict[status] = 0
+
 
 if __name__ == "__main__":
     main()
