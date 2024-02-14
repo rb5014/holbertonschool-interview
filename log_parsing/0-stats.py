114cfddb5dd6e56d32490bfd91875ccb2667527f
Bayle Romain <5014@holbertonstudents.com>
2024-02-14 16:27:21 +0000

    pep8 validation

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,21 +1,34 @@
 #!/usr/bin/python3
 
-"""Write a script that reads stdin line by line and computes metrics:
+"""Write a script that reads stdin line by line
+and computes metrics:
 
-Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size> (if the format is not this one, the line must be skipped)
-After every 10 lines and/or a keyboard interruption (CTRL + C), print these statistics from the beginning:
+Input format:
+<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" \
+<status code> <file size> (if the format is not this one,
+the line must be skipped)
+After every 10 lines and/or a keyboard interruption (CTRL + C),
+print these statistics from the beginning:
 Total file size: File size: <total size>
-where <total size> is the sum of all previous <file size> (see input format above)
+where <total size> is the sum of all previous <file size>
 Number of lines by status code:
 possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-if a status code doesn’t appear or is not an integer, don’t print anything for this status code
+if a status code doesn’t appear or is not an integer,
+don’t print anything for this status code
 format: <status code>: <number>
 status codes should be printed in ascending order
 """
 
 
 tot_file_size = 0
-status_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+status_dict = {'200': 0,
+               '301': 0,
+               '400': 0,
+               '401': 0,
+               '403': 0,
+               '404': 0,
+               '405': 0,
+               '500': 0}
 count = 0
 while 1:
     count += 1
