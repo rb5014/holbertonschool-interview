de7b0a5a34138a21b5a441d834c9727ca773c086
Romain BAYLE <5014@holbertonstudents.com>
2024-04-05 09:45:55 +0000

    pep8 fixed nqueens

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -7,14 +7,17 @@ Usage: nqueens N
 If the user called the program with the wrong number of arguments,
 print Usage: nqueens N, followed by a new line, and exit with the status 1
 where N must be an integer greater or equal to 4
-If N is not an integer, print N must be a number, followed by a new line, and exit with the status 1
-If N is smaller than 4, print N must be at least 4, followed by a new line, and exit with the status 1
+If N is not an integer, print N must be a number, followed by a new line,
+and exit with the status 1
+If N is smaller than 4, print N must be at least 4, followed by a new line,
+and exit with the status 1
 The program should print every possible solution to the problem
 One solution per line
 Format: see example
 You don't have to print the solutions in a specific order
 You are only allowed to import the sys module
 """
+
 
 def queens(n: int, i: int, a: list, b: list, c: list):
     if i < n:
@@ -37,8 +40,5 @@ except ValueError:
     print("N must be a number")
     exit(1)
 
-
-
 for solution in queens(n, 0, [], [], []):
     print(solution)
-
