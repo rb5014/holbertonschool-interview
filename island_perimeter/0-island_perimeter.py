c0a644ab90fc3211929c26bdf219901a6aee9db6
Romain Bayle <5014@holbertonstudents.com>
2024-08-02 13:48:46 +0200

    pep8 fixed island_perimeter

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+
 """
-Create a function def island_perimeter(grid): that returns the perimeter of the island described in grid:
+Create a function def island_perimeter(grid):
+returns the perimeter of the island described in grid:
 
 grid is a list of list of integers:
 0 represents water
@@ -10,21 +12,28 @@ Cells are connected horizontally/vertically (not diagonally).
 grid is rectangular, with its width and height not exceeding 100
 The grid is completely surrounded by water
 There is only one island (or nothing).
-The island doesn t have “lakes” (water inside that isn t connected to the water surrounding the island).
+The island doesn t have “lakes”
+(water inside that isn t connected to the water surrounding the island).
+Args:
+    grid (list): list of rows containing cells
+
+Return: the perimeter
 """
+
+
 def island_perimeter(grid):
-	perimeter = 0
+    perimeter = 0
 
-	for i in range(len(grid)):
-		for j in range(len(grid[i])):
-			if grid[i][j] == 1:
-				if grid[i][j + 1] == 0:
-					perimeter += 1
-				if grid[i][j - 1] == 0:
-					perimeter += 1
-				if grid[i + 1][j] == 0:
-					perimeter += 1
-				if grid[i - 1][j] == 0:
-					perimeter += 1
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if grid[i][j + 1] == 0:
+                    perimeter += 1
+                if grid[i][j - 1] == 0:
+                    perimeter += 1
+                if grid[i + 1][j] == 0:
+                    perimeter += 1
+                if grid[i - 1][j] == 0:
+                    perimeter += 1
 
-	return perimeter
+    return perimeter
